693e01e4b32374da205288cd6cd2d781c1440ef9
ChaHoHoon <co029@naver.com>
2020-09-29 17:12:48 +0900

    QrCode 프로젝트 추가

---
+++ app/src/main/java/com/example/qrcoderead/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.qrcoderead
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.SurfaceHolder
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var detector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        detector = BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.ALL_FORMATS).build()
+        detector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+            }
+
+
+            //결과 디스플레이
+            override fun receiveDetections(p0: Detector.Detections<Barcode>?) {
+                var barcodes = p0?.detectedItems
+                if (barcodes!!.size() > 0) {
+                    val Stringresult = StringBuilder()
+                    resultext.post {
+                        Stringresult.append("결과 :")
+                        Stringresult.append("\n")
+                        Stringresult.append(barcodes.valueAt(0).displayValue)
+                        resultext.text = Stringresult.toString()
+                    }
+                }
+            }
+        })
+
+        cameraSource = CameraSource.Builder(this,detector).setRequestedPreviewSize(1024,768)
+            .setRequestedFps(25f).setAutoFocusEnabled(true).build()
+
+        qr_barcode.holder.addCallback(object : SurfaceHolder.Callback2 {
+            override fun surfaceRedrawNeeded(p0: SurfaceHolder) {
+
+            }
+
+            override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+
+            override fun surfaceDestroyed(p0: SurfaceHolder) {
+                cameraSource.stop()
+            }
+
+            override fun surfaceCreated(p0: SurfaceHolder) {
+                if(ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    cameraSource.start(p0)
+                } else {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.CAMERA), 200)
+                }
+            }
+
+        })
+
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 200){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                cameraSource.start(qr_barcode.holder)
+            } else {
+                Toast.makeText(this, "스캐너는 카메라 권한이 허용되어야 사용 가능합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        detector.release()
+        cameraSource.stop()
+        cameraSource.release()
+    }
+}
