d1a4e7766e8cf38766e1249513926f12ae979638
ChaHoHoon <co029@naver.com>
2020-10-20 10:00:18 +0900

    [수정] Dialog 삭제

---
+++ app/src/main/java/com/example/qrcoderead/ScannerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qrcoderead
+
+import com.journeyapps.barcodescanner.CaptureActivity
+
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+class AnyOrientationCaptureActivity : CaptureActivity() {
+
+}
